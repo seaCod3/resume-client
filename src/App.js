d507262e1ac3788e5c55e10bfed8ddd087823ec5
BrunoCardos <brunocardoso@sapo.cv>
2023-03-18 13:58:48 -0100

    basic project structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <h1>Here We Are!!</h1>
+
+     <header>
+           {/* header */}
+     </header>
+
+     <section className='section'>
+          {/* Features */}
+     </section>
+
+     <section className='section'>
+          {/* FAQ */}
+     </section>
+
+     <section className='section'>
+          {/* Usefull Links */}
+     </section>
+
+     <footer>
+          {/* Footer */}
+     </footer>
+
     </div>
   );
 }
