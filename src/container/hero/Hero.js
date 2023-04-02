861762308da8acba269d3bc24c36183fc16a7c67
BrunoCardos <brunocardoso@sapo.cv>
2023-04-02 13:10:17 -0100

    Add resposivite to hero section

--- src/container/hero/Hero.js
+++ src/container/hero/Hero.js
@@ -1,8 +1,31 @@
 import './hero.css'
 import myImage from './resume-coverletter.webp';
-    ;
 
 export default function Hero() {
+
+
+    window.onload = function () {
+        const mediaQuery = window.matchMedia('(max-width: 991px)')
+      
+        function handleTabletChange(e) {
+          const div = document.querySelector('div.col-sm-12.col-lg-7')
+          if (div === null) {
+            console.log('Element not found!')
+            return
+          }
+          if (e.matches) {
+            div.classList.add('flex-colum')
+          } else {
+            div.classList.remove('flex-colum')
+          }
+        }
+      
+        mediaQuery.addEventListener('change', handleTabletChange)
+        handleTabletChange(mediaQuery)
+      }
+      
+
+
 
     return (
         <div className='hero'>
@@ -10,14 +33,14 @@ export default function Hero() {
 
                 <div className='row' >
                     <div className='col-sm-12 col-lg-7'>
-                        <h1>Create a professional resume and cover letter</h1>
+                        <h1 className='h1'>Create a professional resume and cover letter</h1>
                         <p>Use professional field-tested resume templates that follow the exact ‘resume rules’ employers look for. Easy to use and done within minutes - try now for free!</p>
                         <div className='flex'>
-                        <button className='btn btn-primary hero-btn'>Create a Resume</button>
-                        <button className='btn btn-secondary hero-btn'>Need Some Help?</button>
+                            <button className='btn btn-primary hero-btn'>Create a Resume</button>
+                            <button className='btn btn-secondary hero-btn'>How it works?</button>
                         </div>
                     </div>
-                    <div className='col-sm-12 col-lg-5'>
+                    <div className='col-sm-12 col-lg-5 flex-center'>
                         <img src={myImage} alt='resume cover letter' />
                     </div>
                 </div>
