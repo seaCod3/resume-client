bc92c9156f687dad323d528617feeda196f9e57a
Stefan Vitoria <steph.74lw@gmail.com>
2023-05-13 13:20:22 -0100

    deleted unused files

---
+++ src/container/create-resume/CreateResumePage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const CreateResumePage = () => {
+  return (
+    <div>CreateResumePage</div>
+  )
+}
+
+export default CreateResumePage;
--- src/index.js
+++ src/index.js
@@ -2,16 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import { createBrowserRouter, RouterProvider, } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css"; // Bootstrap CSS
 import "bootstrap/dist/js/bootstrap.bundle.min"; // Bootstrap Bundle JS
+import CreateResumePage from './container/create-resume/CreateResumePage';
 
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+  },
+  {
+    path: "/create-resume",
+    element: <CreateResumePage />,
   },
 ]);
 
@@ -25,7 +29,4 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
