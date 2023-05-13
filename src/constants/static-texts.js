9bb5cd1b2df664630af296e3cb0d746db9b37b8d
Stefan Vitoria <steph.74lw@gmail.com>
2023-05-13 14:42:33 -0100

    changed features texts

--- src/constants/static-texts.js
+++ src/constants/static-texts.js
@@ -4,50 +4,56 @@ import { AiOutlineEdit } from "react-icons/ai";
 
 
 export const features = [
-    {
-      id: "feature-1",
-      icon: <AiOutlineEdit color="#023642" size={30}/>,
-      title: "Edit any misstyping!",
-      content:
-        "Some quick example text to build on the card title and make up the bulk of the card's content. Some quick example text to build on the card title and make up the bulk of the card's content.",
-    },
-    {
-      id: "feature-2",
-      icon: <VscCheckAll color="#023642" size={30}/>,
-      title: "100% Secured!",
-      content:
-      "Some quick example text to build on the card title and make up the bulk of the card's content. Some quick example text to build on the card title and make up the bulk of the card's content.",
-    },
-    {
-      id: "feature-3",
-      icon: <BsDownload color="#023642" size={30}/>,
-      title: "Download any time!",
-      content:
-      "Some quick example text to build on the card title and make up the bulk of the card's content. Some quick example text to build on the card title and make up the bulk of the card's content.",
-    },
-    {
-        id: "feature-6",
-        icon: <BsDownload color="#023642" size={30}/>,
-        title: "Download any time!",
-        content:
-          "Some quick example text to build on the card title and make up the bulk of the card's content. Some quick example text to build on the card title and make up the bulk of the card's content.",
-      },
-      {
-        id: "feature-5",
-        icon: <VscCheckAll color="#023642" size={30}/>,
-        title: "100% Secured!",
-        content:
-          "Some quick example text to build on the card title and make up the bulk of the card's content. Some quick example text to build on the card title and make up the bulk of the card's content.",
-      },
-      {
-        id: "feature-6",
-        icon: <VscCheckAll color="#023642" size={30}/>,
-        title: "100% Secured!",
-        content:
-          "Some quick example text to build on the card title and make up the bulk of the card's content. Some quick example text to build on the card title and make up the bulk of the card's content.",
-      },
-      
-  ];
+  {
+    id: "feature-1",
+    icon: <AiOutlineEdit color="#023642" size={30} />,
+    title: "Easy-to-use Interface",
+    content:
+      `Our CV generator provides a user-friendly interface, making it effortless for users to create professional resumes without any technical expertise. 
+        The intuitive design ensures a smooth and efficient resume-building experience.`,
+  },
+  {
+    id: "feature-2",
+    icon: <VscCheckAll color="#023642" size={30} />,
+    title: "Dynamic Sections",
+    content:
+      `We provide a variety of dynamic sections, such as education, work experience, skills, and more. 
+      Users can easily add, remove, or reorder these sections to showcase their qualifications and achievements effectively.`,
+  },
+  {
+    id: "feature-3",
+    icon: <BsDownload color="#023642" size={30} />,
+    title: "Real-time Preview",
+    content:
+      `Visualize your resume as you build it with our real-time preview feature. 
+      Instantly see how your information and chosen template come together, ensuring a polished and well-structured CV before downloading it.`,
+  },
+  {
+    id: "feature-6",
+    icon: <BsDownload color="#023642" size={30} />,
+    title: "Downloadable PDF",
+    content:
+      `Once you've completed your resume, our CV generator enables you to download it as a high-quality PDF file. 
+          This ensures compatibility across different devices and allows for easy sharing with potential employers or job application platforms.`,
+  },
+  {
+    id: "feature-5",
+    icon: <VscCheckAll color="#023642" size={30} />,
+    title: "Resume Management",
+    content:
+      `Our application provides a built-in resume management system, allowing users to save, edit, and update their resumes anytime. 
+          You can easily access and make changes to your CV whenever necessary, ensuring it remains up-to-date.`,
+  },
+  {
+    id: "feature-6",
+    icon: <VscCheckAll color="#023642" size={30} />,
+    title: "Responsive Design",
+    content:
+      `Create and edit your resume with ease using our CV generator. 
+          It provides a great experience on any device, including desktops, laptops, tablets, and smartphones.`,
+  },
+
+];
 
 
 export const accordionOtpions = [
