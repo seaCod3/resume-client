38cda4d8d8c3a9449d932de42637b7f4f1fdeaa1
Stefan Vitoria <steph.74lw@gmail.com>
2023-07-04 16:30:46 -0100

    refactor: improvements on code structure

--- src/components/form-steps-components/SectionDescriptionCard.js
+++ src/components/form-steps-components/SectionDescriptionCard.js
@@ -9,6 +9,8 @@ const SectionDescriptionCard = ({ title, description }) => {
         {/* <p>Section Description</p> */}
         <h3 className='section-description-custom-card-title' >{title}</h3>
         <p className='section-description-card-paragraph' >{description}</p>
+        {/* <h3 className='custom-card-title' >{title}</h3>
+        <p className='text-start w-100 card-paragraph' >{description}</p> */}
 
       </Stack>
     </Box>
